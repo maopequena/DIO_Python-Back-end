d345b4cd308e069ae3bff410c3a8f035bbbb5378
maopequena <maopequena@gmail.com>
2025-11-06 09:42:04 -0300

    Commits das aulas 1 e 2

---
+++ Aula 2/primeiro_programa.py
@@ -0,0 +1,2 @@
+print("Hello World!")
+print("Meu primeiro programa em Python")
---
+++ Aula 3/tipos_dados.py
@@ -0,0 +1,22 @@
+import math
+
+
+print(1 + 10)
+print(1.5 * 2)
+print(True)
+print(False)
+print("1" + "10")
+print('Olá, Mundo!')
+
+# Classes (passe o mouse para ver a definição):
+int()
+bool()
+float()
+str()
+# Segurar o botão `Ctrl` e clicar nas palavras acima leva você para a implementação dessas classes, que mostra todos os métodos possíveis.
+
+dir()
+import Math
+dir(100)
+
+help()
