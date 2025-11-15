888bb6ed5be37eb616f627dc7e8801f61750d8a7
maopequena <maopequena@gmail.com>
2025-11-15 20:33:39 -0300

    até final aula 9

--- Aula 9/dicionarios.py
+++ Aula 9/dicionarios.py
@@ -37,4 +37,12 @@ print(contatos)
 
 print(contatos["giovana@gmail.com"]["telefone"]) # 3443-2121
 
-# PAREI EM SETDEAFULT
+# VALUES
+contatos1 = {
+    "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"},
+    "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
+    "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
+    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
+}
+
+print(contatos1.values())
